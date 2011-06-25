f2d0dfb78c38105ca17585961da064acd624287b
mixedpuppy <mixedpuppy@gmail.com>
2011-06-24 17:07:32 -0700

    add api_entry to generate json schemas

--- webmailapi/storage.py
+++ webmailapi/storage.py
@@ -1,10 +1,21 @@
 import copy
 from apibase.baseapp import BaseController, api
+from apibase.schema import api_entry, api_param, api_validate
 from lib.imap import ImapStorage
 
 class Storage(BaseController):
 
     @api
+    @api_validate
+    @api_entry(
+        description="get a list of messages in a folder",
+        parameters={
+            'mid': api_param('string', True, None, None, 'path', 'Message ID for message to retreive'),
+            'username': api_param('string', True, None, None, 'query', 'User to login as'),
+            'password': api_param('string', True, None, None, 'query', 'Users password'),
+        },
+        response={'type': 'array',
+                  'description': ('list of message headers (array of strings)')})
     def get_message(self, mid):
         imap = ImapStorage(self.config, self.request.POST)
         return {
@@ -14,9 +25,20 @@ class Storage(BaseController):
 
 
     @api
+    @api_validate
+    @api_entry(
+        description="get a list of messages in a folder",
+        parameters={
+            'name': api_param('string', True, None, None, 'path', 'Folder to get a list from'),
+            'username': api_param('string', True, None, None, 'query', 'User to login as'),
+            'password': api_param('string', True, None, None, 'query', 'Users password'),
+        },
+        response={'type': 'array',
+                  'description': ('list of message headers')})
     def list_messages(self, name):
         data = {'folder': name}
         data.update(self.request.POST)
+        print data
         imap = ImapStorage(self.config, data)
         return {
             'result': imap.list_messages(),
@@ -24,6 +46,15 @@ class Storage(BaseController):
         }
 
     @api
+    @api_validate
+    @api_entry(
+        description="get a list of messages in a folder",
+        parameters={
+            'username': api_param('string', True, None, None, 'query', 'User to login as'),
+            'password': api_param('string', True, None, None, 'query', 'Users password'),
+        },
+        response={'type': 'array',
+                  'description': ('list of folders')})
     def list_folders(self):
         imap = ImapStorage(self.config, self.request.POST)
         f = imap.list_folders()
@@ -33,6 +64,8 @@ class Storage(BaseController):
         }
 
     @api
+    @api_validate
+    @api_entry(description="NOT IMPLEMENTED")
     def send_message(self):
         raise Exception("FAIL WHALE")
 
--- webmailapi/wsgiapp.py
+++ webmailapi/wsgiapp.py
@@ -1,13 +1,39 @@
 
 from apibase.baseapp import *
+from apibase.schema import APIDescription, api_entry, api_param, api_validate, generateSchema
 from routes import Mapper
+from webob.dec import wsgify
+
 from storage import Storage
 
+@api_entry(
+    name='webmailapi',
+    version='v1',
+    description="A REST to IMAP proxy API",
+    title="WebMailAPI",
+    labels=['labs'],
+    icons={'x16': 'https://mozillalabs.com/wp-content/themes/labs2.0/favicon.png'}
+)
+class WebMailAPI(BaseApplication):
+    schema = None
+    @wsgify
+    def __call__(self, req):
+        if not self.schema:
+            generateSchema(self, req)
+        return super(WebMailAPI, self).__call__(req)
+
 map = Mapper()
+with map.submapper(path_prefix='/v1') as v1:
+    v1.connect('message', '/message/{mid}', controller=Storage, action='get_message', conditions=dict(method=["POST"]));
+    v1.connect('message_list', '/folder/{name}', controller=Storage, action='list_messages', conditions=dict(method=["POST"]));
+    v1.connect('folder_list', '/folder', controller=Storage, action='list_folders', conditions=dict(method=["POST"]));
+    v1.connect('message_send', '/send', controller=Storage, action='send_message', conditions=dict(method=["POST"]));
 
 map.connect('message', '/message/{mid}', controller=Storage, action='get_message');
 map.connect('message_list', '/folder/{name}', controller=Storage, action='list_messages');
 map.connect('folder_list', '/folder', controller=Storage, action='list_folders');
 map.connect('message_send', '/send', controller=Storage, action='send_message');
 
-make_app = set_app(map)
+map.connect('/schema', controller=APIDescription, action='schema')
+
+make_app = set_app(map, appKlass=WebMailAPI)
